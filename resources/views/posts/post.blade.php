5667dec4956e5eeaca2d1467dda2ffaf35c3e82e
Milan Glisic <milanglisic@ymail.com>
2017-08-16 02:15:54 +0200

    'Updating'

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -13,4 +13,23 @@
     <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
 
     {{ $post->body }}
+
+
+    <div class="comments">
+        @foreach($post->comments as $comment)
+
+            <ul class="list-group">
+
+                <li class="list-group-item">
+
+                    <strong>{{ $comment->created_at->diffforhumans() }}</strong>
+
+                    {{ $comment->body }}
+
+                </li>
+
+            </ul>
+
+        @endforeach
+    </div>
 </div><!-- /.blog-post -->
